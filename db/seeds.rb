3447a55d4ddca435389990d8a3b40b7db62f55b0
Rami Rizk <ramirizk90@gmail.com>
2021-01-26 17:06:47 +0200

    Add heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create({
+  first_name: 'Rami',
+  last_name: 'Rizk',
+  email: 'rami@example.com',
+  role: 'admin',
+  password_digest: User.digest('0000')
+})
